12b74b4840d8502cc6f1ddf28f139bf47bbf1be2
Misacorp <misa.jokisalo@gmail.com>
2016-01-31 03:05:24 +0200

    Cleaning and commenting

--- chatbot.js
+++ chatbot.js
@@ -11,9 +11,13 @@ sendButton.onclick = function() {
 
 /*  -----------  KIRJOITA APUJUTTUJA ALLE  -----------  */
 
-var vastaus = "";
-var vaihe = 0;
+var vastaus = "";       //  Viimeisin lähetetty viesti
+var vaihe = 0;          //  Missä vaiheessa ohjelmaa mennään / kuinka mones kysyms
 
+//  Ohjelman alussa suoritettavat jutut:
+paivitaVaihe();         
+
+//  Lähetä viesti käyttäjänä.
 function viesti(msg) {
   postMessage(msg);
   vastaus = msg;
@@ -21,14 +25,17 @@ function viesti(msg) {
   clearText(input);
 }
 
+//  Lähetä viesti robottina.
 function koneViesti(msg) {
   viesti("> " + msg);
 }
 
+//  Päivitä ohjelman vaiheen numero nettisivulle.
 function paivitaVaihe() {
   document.getElementById("phaseCounter").innerHTML = vaihe;
 }
 
+//  Vertaa kahta sanaa keskenään.
 function yhtakuin(eka, toka) {
   if(eka.trim().toUpperCase() == toka.trim().toUpperCase()) {
     return true;
@@ -60,11 +67,35 @@ function nappula() {
     if(vastaus < 18) {
       koneViesti("Oletpas nuori!");
     }
-    else {
+    else if (vastaus >= 18) {
       koneViesti("Vanha ja viisas!");
     }
+    else {
+      koneViesti("Kaikenlaista sitä kuulee...");
+    }
     vaihe++;
-    koneViesti("Kuka on paras kaverisi?");
+    koneViesti("Kuinka monta kertaa huudetaan \"Eläköön!\"?");
+    return true;
+  }
+
+  if(vaihe == 2) {
+    var lukumaara = vastaus;
+    for(var a = 0; a < lukumaara; a++) {
+      koneViesti("Eläköön! (" + (a+1) + ")");
+    }
+    vaihe++;
+    koneViesti("Mikä muuten on nimesi?");
+    return true;
+  }
+
+  if(vaihe == 3) {
+    if(yhtakuin(vastaus, "misa")) {
+      koneViesti("Älä huijaa!");
+    }
+    else {
+      koneViesti("Hauska tavata " + vastaus + ".");
+    }
+    vaihe++;
     return true;
   }
 }
