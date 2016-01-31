82feeaa24eb5b7a0211d3c24d126a3e091dce31a
Misacorp <misa.jokisalo@gmail.com>
2016-01-31 02:41:59 +0200

    Sequential question functionality and input comparison

--- chatbot.js
+++ chatbot.js
@@ -2,8 +2,21 @@ var button = document.getElementById("button");
 var input = document.getElementById("input");
 var output = document.getElementById("messages");
 
+sendButton.onclick = function() {
+  viesti(getInput(input));
+  nappula();
+  paivitaVaihe();
+}
+
+
+/*  -----------  KIRJOITA APUJUTTUJA ALLE  -----------  */
+
+var vastaus = "";
+var vaihe = 0;
+
 function viesti(msg) {
   postMessage(msg);
+  vastaus = msg;
   output.scrollTop = output.scrollHeight;
   clearText(input);
 }
@@ -12,9 +25,17 @@ function koneViesti(msg) {
   viesti("> " + msg);
 }
 
-sendButton.onclick = function() {
-  viesti(getInput(input));
+function paivitaVaihe() {
+  document.getElementById("phaseCounter").innerHTML = vaihe;
 }
+
+function yhtakuin(eka, toka) {
+  if(eka.trim().toUpperCase() == toka.trim().toUpperCase()) {
+    return true;
+  }
+  return false;
+}
+
 
 /*  -----------  KIRJOITA OHJELMAKOODI ALLE  -----------  */
 
@@ -22,3 +43,28 @@ sendButton.onclick = function() {
 var aloitusteksti = "Moikka! Mitä kuuluu?"
 output.innerHTML = "> " + aloitusteksti;
 
+function nappula() {
+  if(vaihe == 0) {
+    if(yhtakuin(vastaus, "hyvää")) {
+      koneViesti("Hienoa!");
+    }
+    else {
+      koneViesti("Jaahas...");
+    }
+    vaihe++;
+    koneViesti("Kuinka vanha olet?");
+    return true;
+  }
+
+  if(vaihe == 1) {
+    if(vastaus < 18) {
+      koneViesti("Oletpas nuori!");
+    }
+    else {
+      koneViesti("Vanha ja viisas!");
+    }
+    vaihe++;
+    koneViesti("Kuka on paras kaverisi?");
+    return true;
+  }
+}
