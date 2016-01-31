bd06c2f79d0a4ef93be8d30dd43be070bdccf3a6
Misacorp <misa.jokisalo@gmail.com>
2016-01-31 10:57:30 +0200

    Removed program content to later be written in by students
    For an examples of program content see the master branch.

--- chatbot.js
+++ chatbot.js
@@ -15,7 +15,7 @@ var vastaus = "";       //  Viimeisin lähetetty viesti
 var vaihe = 0;          //  Missä vaiheessa ohjelmaa mennään / kuinka mones kysyms
 
 //  Ohjelman alussa suoritettavat jutut:
-paivitaVaihe();         
+paivitaVaihe();
 
 //  Lähetä viesti käyttäjänä.
 function viesti(msg) {
@@ -50,52 +50,3 @@ function yhtakuin(eka, toka) {
 var aloitusteksti = "Moikka! Mitä kuuluu?"
 output.innerHTML = "> " + aloitusteksti;
 
-function nappula() {
-  if(vaihe == 0) {
-    if(yhtakuin(vastaus, "hyvää")) {
-      koneViesti("Hienoa!");
-    }
-    else {
-      koneViesti("Jaahas...");
-    }
-    vaihe++;
-    koneViesti("Kuinka vanha olet?");
-    return;
-  }
-
-  if(vaihe == 1) {
-    if(vastaus < 18) {
-      koneViesti("Oletpas nuori!");
-    }
-    else if (vastaus >= 18) {
-      koneViesti("Vanha ja viisas!");
-    }
-    else {
-      koneViesti("Kaikenlaista sitä kuulee...");
-    }
-    vaihe++;
-    koneViesti("Kuinka monta kertaa huudetaan \"Eläköön!\"?");
-    return;
-  }
-
-  if(vaihe == 2) {
-    var lukumaara = vastaus;
-    for(var a = 0; a < lukumaara; a++) {
-      koneViesti("Eläköön! (" + (a+1) + ")");
-    }
-    vaihe++;
-    koneViesti("Mikä muuten on nimesi?");
-    return;
-  }
-
-  if(vaihe == 3) {
-    if(yhtakuin(vastaus, "misa")) {
-      koneViesti("Älä huijaa!");
-    }
-    else {
-      koneViesti("Hauska tavata " + vastaus + ".");
-    }
-    vaihe++;
-    return;
-  }
-}
